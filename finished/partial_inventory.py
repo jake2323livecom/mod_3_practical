8b0df6fa3c287c145ede83e35b27008285328197
blah <blah@gmail.com>
2022-10-06 01:36:48 -0400

    partial

--- finished/partial_inventory.py
+++ finished/partial_inventory.py
@@ -7,15 +7,21 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Step 1 - Build API call variables
-NAUTOBOT_TOKEN = None
-DEVICES_API_URL = None
-METHOD = None
-HEADERS = None
-PARAMETERS = None
+NAUTOBOT_TOKEN = '89290b6b9df1226fa76fac388c3dc3f6a2a56d48'
+DEVICES_API_URL = 'https://10.10.44.21/api/dcim/devices'
+METHOD = 'GET'
+HEADERS = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': f'Token {NAUTOBOT_TOKEN}'
+}
+PARAMETERS = {
+    'site': 'orko_mod_3_practical'
+}
 
 # Step 2 - Execute the API call
-devices = None
-devices_json = None
+devices = requests.request(method=METHOD, url=DEVICES_API_URL, headers=HEADERS, params=PARAMETERS)
+devices_json = devices.json()
 
 # Step 3 - Add hosts to hostvars
 hostvars = {
